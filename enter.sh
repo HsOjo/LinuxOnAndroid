5f3966261d871af9d8a3dbc01d31effad1bc3847
HsOjo <1134031392@qq.com>
2026-08-01 04:20:11 +0800

    Start container with a clean environment.

--- enter.sh
+++ enter.sh
@@ -11,12 +11,12 @@ HAVE_CAPSH=0
 "$BB" nsenter -t "$P" -m -u -- /bin/sh -c '[ -x /usr/sbin/capsh ]' 2>/dev/null && HAVE_CAPSH=1
 if [ "$#" -gt 0 ]; then
   if [ "$HAVE_CAPSH" = 1 ]; then
-    exec "$BB" nsenter -t "$P" -m -u -- /usr/sbin/capsh --user=root --drop=cap_sys_ptrace --shell=/bin/sh -- -c 'PATH=/bin:/sbin:/usr/bin:/usr/sbin; export PATH; "$@"' enter "$@"
+    exec "$BB" nsenter -t "$P" -m -u -- /usr/sbin/capsh --user=root --drop=cap_sys_ptrace --shell=/bin/sh -- -c 'exec /usr/bin/env -i PATH=/bin:/sbin:/usr/bin:/usr/sbin HOME=/root TMPDIR=/tmp "$@"' enter "$@"
   fi
-  exec "$BB" nsenter -t "$P" -m -u -- /bin/sh -c 'PATH=/bin:/sbin:/usr/bin:/usr/sbin; export PATH; "$@"' enter "$@"
+  exec "$BB" nsenter -t "$P" -m -u -- /usr/bin/env -i PATH=/bin:/sbin:/usr/bin:/usr/sbin HOME=/root TMPDIR=/tmp "$@"
 else
   if [ "$HAVE_CAPSH" = 1 ]; then
-    exec "$BB" nsenter -t "$P" -m -u -- /usr/sbin/capsh --user=root --drop=cap_sys_ptrace --shell=/bin/sh -- -c 'PATH=/bin:/sbin:/usr/bin:/usr/sbin; export PATH; exec /bin/sh'
+    exec "$BB" nsenter -t "$P" -m -u -- /usr/sbin/capsh --user=root --drop=cap_sys_ptrace --shell=/bin/sh -- -c 'exec /usr/bin/env -i PATH=/bin:/sbin:/usr/bin:/usr/sbin HOME=/root TMPDIR=/tmp /bin/sh'
   fi
-  exec "$BB" nsenter -t "$P" -m -u -- /bin/sh -c 'PATH=/bin:/sbin:/usr/bin:/usr/sbin; export PATH; exec /bin/sh'
+  exec "$BB" nsenter -t "$P" -m -u -- /usr/bin/env -i PATH=/bin:/sbin:/usr/bin:/usr/sbin HOME=/root TMPDIR=/tmp /bin/sh
 fi
